8e90855006f331be9e887c8813056c3fceee3445
ZJUZQ <zhouqiang@zju.edu.cn>
2017-12-01 01:51:01 +0800

    revise install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+cd ~
 git clone https://github.com/ZJUZQ/BOOSTING_lib.git
-cd BOOSTING_lib
+cd ~/BOOSTING_lib
 chmod +x build.sh
 ./build.sh
 
 mkdir -p ~/workspace/ros/catkin/src
 cd ~/workspace/ros/catkin
 catkin_make --force-cmake
-rosws init ~/workspace/ros/catkin/devel
+#rosws init ~/workspace/ros/catkin/devel
 echo "source ~/workspace/ros/catkin/devel/setup.bash" >> ~/.bashrc
 
 cd ~/workspace/ros/catkin/src
